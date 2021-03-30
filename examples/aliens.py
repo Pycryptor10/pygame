44083b1ffb9ec3646d802377918b887721d0a866
Pycryptor10 <47485078+Pycryptor10@users.noreply.github.com>
2021-03-30 21:04:26 +0100

    Update aliens.py

--- examples/aliens.py
+++ examples/aliens.py
@@ -25,6 +25,7 @@ Controls
 
 import random
 import os
+import sys
 
 # import basic pygame modules
 import pygame as pg
@@ -44,6 +45,25 @@ SCORE = 0
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
+import sys
+
+def trace_calls(frame, event, arg):
+    if event != 'call':
+        return
+    co = frame.f_code
+    func_name = co.co_name
+    if func_name == 'write':
+        # Ignore write() calls from print statements
+        return
+    func_line_no = frame.f_lineno
+    func_filename = co.co_filename
+    caller = frame.f_back
+    caller_line_no = caller.f_lineno
+    caller_filename = caller.f_code.co_filename
+    print('Call to %s on line %s of %s from line %s of %s' % \
+        (func_name, func_line_no, func_filename,
+         caller_line_no, caller_filename))
+    return
 
 def load_image(file):
     """ loads an image, prepares it for play
@@ -407,5 +427,6 @@ def main(winstyle=0):
 
 # call the "main" function if running this script
 if __name__ == "__main__":
+    sys.settrace(trace_calls)
     while True:
         main()
